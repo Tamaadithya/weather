d624d1f6099351d41df40f9cf49cf697d48c38c4
Tamaadithya <adithyaputra2503@gmail.com>
2024-06-06 21:38:04 +0700

    90% done

--- weather.js
+++ weather.js
@@ -2,54 +2,341 @@ const CurrentWeatherCard = document.querySelectorAll('.Weather-kiri .card')[0];
 const CityInput = document.getElementById('city_input');
 const searchBtn = document.getElementById('searchBtn');
 
-function getWeatherDetails(name, lat, lon, country, state){
-  const WEATHER_API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto`;
-  const days = ['sun','mon', 'tue', 'wed','thu', 'fri', 'sat'];
-  const months =  ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'DeC'];
-
-  fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
-    let date = new Date();
-    let today = date.getDate();
-    let weatherCode = data.daily.weathercode[0];
-    let tempMax = data.daily.temperature_2m_max[0];
-    let tempMin = data.daily.temperature_2m_min[0];
-
-    CurrentWeatherCard.innerHTML = `
-        <div class="Weather-today">
-            <div class="details">
-                <p>Hari Ini</p>
-                <h2>${(data.main.temp - 273.15).toFixed(2)}&deg;C</h2>
-                <p>${data.weather[0].description}</p>
-            </div>
-            <div class="Weather-icon">
-                <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-            </div>
-            </div>
-            <hr>
-            <div class="Card-footer">
-                <p><i class="fa fa-map-marker"></i>${days[date.getDay()]}, ${date.getDate()}, ${months[date.getMonth()]} ${date.getFullYear()}</p>
-                <p><i class="fa fa-calendar"></i> ${name}, ${country}</p>
-            </div>
-    `;
-  }).catch(() => {
-    alert('failed to fetch current weather');
-  })
+let wmo = {
+  "0": {
+      "day": {
+          "description": "Sunny",
+          "image": "http://openweathermap.org/img/wn/01d@2x.png"
+      },
+      "night": {
+          "description": "Clear",
+          "image": "http://openweathermap.org/img/wn/01n@2x.png"
+      }
+  },
+  "1": {
+      "day": {
+          "description": "Mainly Sunny",
+          "image": "http://openweathermap.org/img/wn/01d@2x.png"
+      },
+      "night": {
+          "description": "Mainly Clear",
+          "image": "http://openweathermap.org/img/wn/01n@2x.png"
+      }
+  },
+  "2": {
+      "day": {
+          "description": "Partly Cloudy",
+          "image": "http://openweathermap.org/img/wn/02d@2x.png"
+      },
+      "night": {
+          "description": "Partly Cloudy",
+          "image": "http://openweathermap.org/img/wn/02n@2x.png"
+      }
+  },
+  "3": {
+      "day": {
+          "description": "Cloudy",
+          "image": "http://openweathermap.org/img/wn/03d@2x.png"
+      },
+      "night": {
+          "description": "Cloudy",
+          "image": "http://openweathermap.org/img/wn/03n@2x.png"
+      }
+  },
+  "45": {
+      "day": {
+          "description": "Foggy",
+          "image": "http://openweathermap.org/img/wn/50d@2x.png"
+      },
+      "night": {
+          "description": "Foggy",
+          "image": "http://openweathermap.org/img/wn/50n@2x.png"
+      }
+  },
+  "48": {
+      "day": {
+          "description": "Rime Fog",
+          "image": "http://openweathermap.org/img/wn/50d@2x.png"
+      },
+      "night": {
+          "description": "Rime Fog",
+          "image": "http://openweathermap.org/img/wn/50n@2x.png"
+      }
+  },
+  "51": {
+      "day": {
+          "description": "Light Drizzle",
+          "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+          "description": "Light Drizzle",
+          "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+  },
+  "53": {
+      "day": {
+          "description": "Drizzle",
+          "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+          "description": "Drizzle",
+          "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+  },
+  "55": {
+      "day": {
+          "description": "Heavy Drizzle",
+          "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+          "description": "Heavy Drizzle",
+          "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+  },
+  "56": {
+      "day": {
+          "description": "Light Freezing Drizzle",
+          "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+          "description": "Light Freezing Drizzle",
+          "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+  },
+  "57": {
+      "day": {
+          "description": "Freezing Drizzle",
+          "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+          "description": "Freezing Drizzle",
+          "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+  },
+  "61": {
+      "day": {
+          "description": "Light Rain",
+          "image": "http://openweathermap.org/img/wn/10d@2x.png"
+      },
+      "night": {
+          "description": "Light Rain",
+          "image": "http://openweathermap.org/img/wn/10n@2x.png"
+      }
+  },
+  "63": {
+      "day": {
+          "description": "Rain",
+          "image": "http://openweathermap.org/img/wn/10d@2x.png"
+      },
+      "night": {
+          "description": "Rain",
+          "image": "http://openweathermap.org/img/wn/10n@2x.png"
+      }
+  },
+  "65": {
+      "day": {
+          "description": "Heavy Rain",
+          "image": "http://openweathermap.org/img/wn/10d@2x.png"
+      },
+      "night": {
+          "description": "Heavy Rain",
+          "image": "http://openweathermap.org/img/wn/10n@2x.png"
+      }
+  },
+  "66": {
+      "day": {
+          "description": "Light Freezing Rain",
+          "image": "http://openweathermap.org/img/wn/10d@2x.png"
+      },
+      "night": {
+          "description": "Light Freezing Rain",
+          "image": "http://openweathermap.org/img/wn/10n@2x.png"
+      }
+  },
+  "67": {
+      "day": {
+          "description": "Freezing Rain",
+          "image": "http://openweathermap.org/img/wn/10d@2x.png"
+      },
+      "night": {
+          "description": "Freezing Rain",
+          "image": "http://openweathermap.org/img/wn/10n@2x.png"
+      }
+  },
+  "71": {
+      "day": {
+          "description": "Light Snow",
+          "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+          "description": "Light Snow",
+          "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+  },
+  "73": {
+      "day": {
+          "description": "Snow",
+          "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+          "description": "Snow",
+          "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+  },
+  "75": {
+      "day": {
+          "description": "Heavy Snow",
+          "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+          "description": "Heavy Snow",
+          "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+  },
+  "77": {
+      "day": {
+          "description": "Snow Grains",
+          "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+          "description": "Snow Grains",
+          "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+  },
+  "80": {
+      "day": {
+          "description": "Light Showers",
+          "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+          "description": "Light Showers",
+          "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+  },
+  "81": {
+      "day": {
+          "description": "Showers",
+          "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+          "description": "Showers",
+          "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+  },
+  "82": {
+      "day": {
+          "description": "Heavy Showers",
+          "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+          "description": "Heavy Showers",
+          "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+  },
+  "85": {
+      "day": {
+          "description": "Light Snow Showers",
+          "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+          "description": "Light Snow Showers",
+          "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+  },
+  "86": {
+      "day": {
+          "description": "Snow Showers",
+          "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+          "description": "Snow Showers",
+          "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+  },
+  "95": {
+      "day": {
+          "description": "Thunderstorm",
+          "image": "http://openweathermap.org/img/wn/11d@2x.png"
+      },
+      "night": {
+          "description": "Thunderstorm",
+          "image": "http://openweathermap.org/img/wn/11n@2x.png"
+      }
+  },
+  "96": {
+      "day": {
+          "description": "Light Thunderstorms With Hail",
+          "image": "http://openweathermap.org/img/wn/11d@2x.png"
+      },
+      "night": {
+          "description": "Light Thunderstorms With Hail",
+          "image": "http://openweathermap.org/img/wn/11n@2x.png"
+      }
+  },
+  "99": {
+      "day": {
+          "description": "Thunderstorm With Hail",
+          "image": "http://openweathermap.org/img/wn/11d@2x.png"
+      },
+      "night": {
+          "description": "Thunderstorm With Hail",
+          "image": "http://openweathermap.org/img/wn/11n@2x.png"
+      }
+  }
 }
-
-
-
-
-function getCityCoordinates() {
+async function getCityCoordinates() {
+  document.getElementById(`sevendays`).innerHTML = ``
   let cityName = CityInput.value.trim();
   CityInput.value = '';
   if (!cityName) return;
   let GEOCODING_API_URL = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}`;
-  fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-    let {name, lat, lon, country, state} = data[0];
-    getWeatherDetails(name,lat,lon,country, state)
-  }).catch(()=>{
-    alert(`failed to fetch coordinates of ${cityName}`)
-  });
+  const response = await fetch(GEOCODING_API_URL)
+  const data = await response.json();
+    console.log(data,`ini data contoh`) 
+  const response7days = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${data.results[0].latitude}&longitude=${data.results[0].longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min`)
+  const data7days = await response7days.json();
+    console.log(data7days, `for loop`);
+  for (let i = 0; i < data7days.daily.temperature_2m_max.length; i++) {
+    document.getElementById(`sevendays`).innerHTML += `
+    <div class="card">
+    <p>${data7days.daily.time[i]}</p>
+    <img src=${wmo[data7days.daily.weather_code[i]].day.image} alt="${wmo[data7days.daily.weather_code[i]].day.description}">
+    <p>${data7days.daily.temperature_2m_max[i]}&deg;C</p>
+  </div>`  
+  }
+  const responseHumidty = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${data.results[0].latitude}&longitude=${data.results[0].longitude}&current=temperature_2m,relative_humidity_2m,weather_code,pressure_msl,wind_speed_10m`)
+  const Humidty = await responseHumidty.json();
+    console.log(Humidty, `humidity`);
+      document.getElementById(`Humidity`).innerHTML = Humidty.current.relative_humidity_2m
+      document.getElementById(`current-temperature`).innerHTML = Humidty.current.temperature_2m+`&deg;C`
+      document.getElementById(`imageweather`).innerHTML = `<img src=${wmo[Humidty.current.weather_code].day.image} alt="${wmo[Humidty.current.weather_code].day.description}"></img>`
 }
 
 searchBtn.addEventListener('click', getCityCoordinates);
+async function fetchWeather(){
+let GEOCODING_API_URL = `https://geocoding-api.open-meteo.com/v1/search?name=Jakarta`;
+const response = await fetch(GEOCODING_API_URL)
+const data = await response.json();
+  console.log(data,`ini data contoh`) 
+const response7days = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${data.results[0].latitude}&longitude=${data.results[0].longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min`)
+const data7days = await response7days.json();
+  console.log(data7days, `for loop`);
+for (let i = 0; i < data7days.daily.temperature_2m_max.length; i++) {
+  document.getElementById(`sevendays`).innerHTML += `
+  <div class="card">
+  <p>${data7days.daily.time[i]}</p>
+  <img src=${wmo[data7days.daily.weather_code[i]].day.image} alt="${wmo[data7days.daily.weather_code[i]].day.description}">
+  <p>${data7days.daily.temperature_2m_max[i]}&deg;C</p>
+</div>` 
+
+const responseHumidty = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${data.results[0].latitude}&longitude=${data.results[0].longitude}&current=temperature_2m,relative_humidity_2m,weather_code,pressure_msl,wind_speed_10m`)
+  const Humidty = await responseHumidty.json();
+    console.log(Humidty, `humidity`);
+      document.getElementById(`Humidity`).innerHTML = Humidty.current.relative_humidity_2m
+      document.getElementById(`current-temperature`).innerHTML = Humidty.current.temperature_2m+`&deg;C`
+      document.getElementById(`imageweather`).innerHTML = `<img src=${wmo[Humidty.current.weather_code].day.image} alt="${wmo[Humidty.current.weather_code].day.description}"></img>`
+}
+
+}
+fetchWeather()
+
